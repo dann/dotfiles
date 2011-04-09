8d8a73e70f83d77c3e1ae6e77d6157ae885f70b4
Dann <techmemo@gmail.com>
2011-04-09 23:08:33 +0900

    extract smart

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -31,21 +31,26 @@ function cdf () {
 
 # Extract archive
 function extract() {
-    case $1 in
-        *.tar.gz|*.tgz) tar xzvf $1 ;;
-        *.tar.xz) tar Jxvf $1 ;;
-        *.zip) unzip $1 ;;
-        *.lzh) lha e $1 ;;
-        *.tar.bz2|*.tbz) tar xjvf $1 ;;
-        *.tar.Z) tar zxvf $1 ;;
-        *.gz) gzip -dc $1 ;;
-        *.bz2) bzip2 -dc $1 ;;
-        *.Z) uncompress $1 ;;
-        *.tar) tar xvf $1 ;;
-        *.arj) unarj $1 ;;
-    esac
+    if [ -f $1 ]; then
+        case $1 in
+            *.Z)        uncompress $1       ;;
+            *.bz2)      bunzip2 $1          ;;
+            *.dmg)      hdiutil mount $1    ;;
+            *.gz)       gunzip $1           ;;
+            *.lzh)      lha e $1            ;;
+            *.tar)      tar -xvf $1         ;;
+            *.tar.bz2)  tar -jxvf $1        ;;
+            *.tar.gz|*.tgz) tar xzvf $1     ;;
+            *.tar.xz)   tar Jxvf $1         ;;
+            *.tbz2)     tar -jxvf $1        ;;
+            *.zip)      unzip $1            ;;
+            *)          echo "'$1' cannot be extracted/mounted via extract()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
 }
-alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
+alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz,dmg}=extract
 
 #-------------------------------
 # Perl
