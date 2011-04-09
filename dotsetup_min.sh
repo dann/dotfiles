4bdc175c7749cccb32d5c48f0ca55664798052ea
Dann <techmemo@gmail.com>
2011-04-09 21:26:21 +0900

    refactored

--- dotsetup_min.sh
+++ dotsetup_min.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm ~/.zshrc ~/.zshrc-minimum ~/.screenrc ~/.vimrc  ~/.vimrc-minimum ~/.Xdefaults  ~/.bashrc ~/.bash_profile ~/.bashrc-minimum ~/.bash
+rm ~/.zshrc ~/.screenrc ~/.vimrc ~/.Xdefaults  ~/.bashrc ~/.bash_profile ~/.bash
 
 CURRENT_DIR=`pwd`
-ln -s $CURRENT_DIR/.zshrc-minimum ~/.zshrc
+ln -s $CURRENT_DIR/.zsh/00-core.zsh ~/.zshrc
 ln -s $CURRENT_DIR/.screenrc ~/.screenrc
-ln -s $CURRENT_DIR/.vimrc-minimum ~/.vimrc
+ln -s $CURRENT_DIR/.vim/vimrc-minimum ~/.vimrc
 ln -s $CURRENT_DIR/.bash ~/.bash
 ln -s $CURRENT_DIR/.bashrc_profile ~/.bashrc_profile
-ln -s $CURRENT_DIR/.bashrc-minimum ~/.bashrc
+ln -s $CURRENT_DIR/.bashrc ~/.bashrc
 
