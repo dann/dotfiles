fa1486e806941e01243f51292eb78cabe0632f72
Dann <techmemo@gmail.com>
2011-04-10 00:00:24 +0900

    align

---
+++ dotsetup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rm ~/.zshrc ~/.zshrc-minimum ~/.zshrc-main ~/.screenrc ~/.vimrc ~/.irbrc ~/.ctags ~/.vim ~/devbin ~/.zsh ~/.my.cnf ~/.dev_users ~/.devenvrc ~/.vimrc-before ~/.tidyrc ~/.perltidyrc  ~/.gitconfig  ~/.zshrc-mac ~/.vimrc-before ~/.vimrc-mac ~/.vimrc-minimum ~/.vimrc-plugins ~/.Xdefaults ~/.zshrc-java ~/.noserc ~/.pylint ~/.bashrc ~/.bash_profile ~/.bashrc-minimum  ~/.bashrc-mac ~/.bash ~/.shipit ~/.tscreenrc ~/.gdbinit
+
+CURRENT_DIR=`pwd`
+
+ln -s $CURRENT_DIR/.Xdefaults       ~/.Xdefaults
+ln -s $CURRENT_DIR/.bash            ~/.bash
+ln -s $CURRENT_DIR/.bashrc          ~/.bashrc
+ln -s $CURRENT_DIR/.bashrc_profile  ~/.bashrc_profile
+ln -s $CURRENT_DIR/.ctags           ~/.ctags
+ln -s $CURRENT_DIR/.dev_users       ~/.dev_users
+ln -s $CURRENT_DIR/.devenvrc        ~/.devenvrc
+ln -s $CURRENT_DIR/.gdbinit         ~/.gdbinit
+ln -s $CURRENT_DIR/.gitconfig       ~/.gitconfig
+ln -s $CURRENT_DIR/.irbrc           ~/.irbrc
+ln -s $CURRENT_DIR/.irssi           ~/.irssi
+ln -s $CURRENT_DIR/.my.cnf          ~/.my.cnf
+ln -s $CURRENT_DIR/.noserc          ~/.noserc
+ln -s $CURRENT_DIR/.perltidyrc      ~/.perltidyrc
+ln -s $CURRENT_DIR/.pylint          ~/.pylint
+ln -s $CURRENT_DIR/.screenrc        ~/.screenrc
+ln -s $CURRENT_DIR/.shipit          ~/.shipit
+ln -s $CURRENT_DIR/.tidyrc          ~/.tidyrc
+ln -s $CURRENT_DIR/.tscreenrc       ~/.tscreenrc
+ln -s $CURRENT_DIR/.vim             ~/.vim
+ln -s $CURRENT_DIR/.vimrc           ~/.vimrc
+ln -s $CURRENT_DIR/.vimrc-before    ~/.vimrc-before
+ln -s $CURRENT_DIR/.zsh             ~/.zsh
+ln -s $CURRENT_DIR/.zshrc           ~/.zshrc
+ln -s $CURRENT_DIR/devbin           ~/devbin
+ln -s $CURRENT_DIR/devtools         ~/devtools
+
+chmod 700 ~/devbin/*
