f549d3d42cca7f92465bedf80d500e0fcca81041
Dann <techmemo@gmail.com>
2013-05-02 16:52:42 +0900

    anything

--- .emacs.d/conf/40-anything.el
+++ .emacs.d/conf/40-anything.el
@@ -1,35 +1,99 @@
-(require 'anything-startup)
+(when (require 'anything nil t)
+  (setq
+   ;; 候補を表示するまでの時間。デフォルトは0.5
+   anything-idle-delay 0.2
+   ;; タイプして再描写するまでの時間。デフォルトは0.1
+   anything-input-idle-delay 0.1
+   ;; 候補の最大表示数。デフォルトは50
+   anything-candidate-number-limit 50
+   ;; 候補が多いときに体感速度を早くする
+   anything-quick-update t
+   ;; 候補選択ショートカットをアルファベットに
+   anything-enable-shortcuts 'alphabet)
 
-(setq
- ;; 候補を表示するまでの時間。デフォルト0.5秒
- anything-idle-delay 0.3
- ;; 候補がおおいときに体感速度を早く
- anything-quick-update t)
+  ;; anything-c-source-emacs-functionsの高速化
+  (setq anything-c-source-emacs-functions
+        '((name . "Emacs Functions")
+          (init .  (lambda ()
+                     (with-current-buffer (anything-candidate-buffer 'global)
+                       (insert
+                        (mapconcat
+                         'identity
+                         (let (commands)
+                           (mapatoms (lambda (a)
+                                       (if (functionp a)
+                                           (push (symbol-name a) commands))))
+                           (sort commands 'string-lessp))
+                         "\n")))))
+          (candidates-in-buffer)
+          (type . function)
+          (requires-pattern . 2)))
 
-(define-key global-map (kbd "C-x b") 'anything)
+  (when (require 'anything-config nil t)
+    (setq anything-sources
+          '(anything-c-source-buffers
+            anything-c-source-recentf
+            anything-c-source-man-pages
+            anything-c-source-emacs-commands
+            anything-c-source-emacs-functions
+            anything-c-source-files-in-current-dir
+            ))
 
-;; M-y で anything-show-kill-ring
-(define-key global-map (kbd "M-y") 'anything-show-kill-ring)
+    ;; root権限でアクションを実行するときのコマンド
+    ;; デフォルトは"su"
+    (setq anything-su-or-sudo "sudo"))
 
-;; anything-complete
-(anything-read-string-mode 1)
+  (require 'anything-match-plugin nil t)
+  (and (equal current-language-environment "Japanese")
+       (executable-find "cmigemo")
+       (require 'anything-migemo nil t))
+  (when (require 'anything-complete nil t)
+    ;; M-xによる補完をAnythingで行なう
+    ;; (anything-read-string-mode 1)
+    ;; lispシンボルの補完候補の再検索時間
+    (anything-lisp-complete-symbol-set-timer 150))
 
-(define-key emacs-lisp-mode-map "\C-\M-i" 'anything-lisp-complete-symbol-partial-match)
-(define-key lisp-interaction-mode-map "\C-\M-i" 'anything-lisp-complete-symbol-partial-match)
+  (require 'anything-show-completion nil t)
 
-;; filelist
-(defun anything-custom-filelist ()
+  (when (require 'auto-install nil t)
+    (require 'anything-auto-install nil t))
+
+  (when (require 'descbinds-anything nil t)
+    ;; describe-bindingsをAnythingに置き換える
+    (descbinds-anything-install))
+
+  (require 'anything-grep nil t)
+
+  ;;; P88
+  ;;; manやinfoを調べるコマンドを作成してみる
+  ;; anything-for-document 用のソースを定義
+  (setq anything-for-document-sources
+      (list anything-c-source-man-pages
+            anything-c-source-info-cl
+            anything-c-source-info-pages
+            anything-c-source-info-elisp
+            anything-c-source-apropos-emacs-commands
+            anything-c-source-apropos-emacs-functions
+            anything-c-source-apropos-emacs-variables))
+  ;; anything-for-document コマンドを作成
+  (defun anything-for-document ()
+    "Preconfigured `anything' for anything-for-document."
     (interactive)
-    (anything-other-buffer
-     (append
-      '(anything-c-source-ffap-line
-        anything-c-source-ffap-guesser
-        anything-c-source-buffers+
-        )
-      (anything-c-sources-git-project-for)
-      '(anything-c-source-recentf
-        anything-c-source-bookmarks
-        anything-c-source-file-cache
-        anything-c-source-filelist
-        ))
-     "*anything file list*"))
+    (anything anything-for-document-sources (thing-at-point 'symbol) nil nil nil "*anything for document*"))
+
+  ;;; P90
+  ;;; anything-project: プロジェクトからファイルを絞り込み
+  ;; (install-elisp "http://github.com/imakado/anything-project/raw/master/anything-project.el")
+  ;; (when (require 'anything-project nil t)
+  ;;   (setq ap:project-files-filters
+  ;;         (list
+  ;;          (lambda (files)
+  ;;            (remove-if 'file-directory-p files)
+  ;;            (remove-if '(lambda (file) (string-match-p "~$" file)) files)))))
+
+  (require 'anything-migemo)
+
+  ;; (require 'anything-hatena-bookmark)
+
+  ;; C-hで一文字削除になるように
+  (define-key anything-map (kbd "C-h") 'delete-backward-char))
