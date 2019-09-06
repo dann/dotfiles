18680eeeb2077a5a652c8b16aa8e34e1cf84fb95
Dann <techmemo@gmail.com>
2019-09-07 01:07:16 +0900

    removed unused color

--- .zsh/00_core.zsh
+++ .zsh/00_core.zsh
@@ -11,7 +11,6 @@ export VISUAL=vim
 export COLOR=32
 export SCREENCOLOR=g
 export PERL_BADLANG=0
-export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 export LESS="-erX"
 
 # save history
