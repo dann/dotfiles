d56836fb924636e92932701cfe497cdfe8841a79
Dann <techmemo@gmail.com>
2019-09-07 00:27:51 +0900

    local

--- dotsetup.sh
+++ dotsetup.sh
@@ -37,7 +37,7 @@ devtools
 setup() {
     remove_dotfiles
     link_dotfiles
-    setup_language_servers
+    setup_local_dir
 }
 
 remove_dotfiles() {
@@ -55,11 +55,8 @@ link_dotfiles() {
     done
 }
 
-
-setup_language_servers() {
-    go get golang.org/x/tools/cmd/gopls
-    pip3 install python-language-server
-    npm install -g typescript typescript-language-server
+setup_local_dir() {
+    mkdir $HOME/local
 }
 
 setup
